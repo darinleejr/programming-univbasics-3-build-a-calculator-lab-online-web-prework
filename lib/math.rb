dd193435706ee566f1bfc17fdfd92e924a0f5f7d
Darin Webster <darinleenavy@gmail.com>
2019-06-11 19:05:54 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,31 @@
 def addition(num1, num2)
-
+ puts "sum"
+ sum = {num1}+{num2}
 end
 
 def subtraction(num1, num2)
-
+ puts "differnece"  
+ difference = {num1}-{num2}
 end
 
 def division(num1, num2)
-
+ puts "quotient"
+ quotient = {num1}/{num2}
 end
 
 def multiplication(num1, num2)
-
+ puts "product"
+ product = {num1}*{num2}
 end
 
-def modulo(num1, num2)
-
+def modulo(num1, num2) do
+ it "returns the remainder of two numbers" do
+   expect(modulo(34, 5)).to eq(4)
+ end
 end
 
-def square_root(num)
-
+def square_root(num) do
+ it "returns the square root of a number" do
+   expect(square_root(81)).to eq (9)
+  end
 end
